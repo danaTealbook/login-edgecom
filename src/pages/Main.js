880a629cc0c4abbbbabdc0f80bd7ae76191d4faa
Dana Ken <dana@tealbook.com>
2023-12-11 15:35:51 -0500

    cleaned Main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -73,18 +73,6 @@ export default function Main() {
     return index;
   };
 
-  const createId = () => {
-    let id = "";
-    let chars =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (let i = 0; i < 5; i++) {
-      id += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-
-    return id;
-  };
-
   const handleSaveArticle = () => {
     setSubmitted(true);
 
